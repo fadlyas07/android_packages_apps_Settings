6ff83d1dc8234706cd2824480b61104f933d7f0a
XayahSuSuSu <zds1249475336@gmail.com>
2022-04-24 01:44:35 +0800

    Settings: Add acme version to firmware version page

---
+++ src/com/android/settings/deviceinfo/firmwareversion/AcmeVersionPreferenceController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2022 Acme
+ *
+ * PDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class AcmeVersionPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "AcmeVersionPreferenceController";
+
+    private static final String KEY_ACME_BRANCH_PROP = "ro.acme.branch";
+
+    private static final String KEY_ACME_BUILD_TYPE_PROP = "ro.acme.build.type";
+
+    public AcmeVersionPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String version = SystemProperties.get(KEY_ACME_BRANCH_PROP, mContext.getString(R.string.unknown)).toUpperCase() + " | " + SystemProperties.get(KEY_ACME_BUILD_TYPE_PROP, mContext.getString(R.string.unknown)).toUpperCase();
+        return version;
+    }
+}
